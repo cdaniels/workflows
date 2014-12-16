a8e410018239c8d1e3af411e807b9de50efc99fd
cdaniels <cdaniels@marlboro.edu>
2014-12-16 13:01:08 -0500

    added coffeecript files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,16 @@
-var gulp = require('gulp');
-gutil = require('gulp-util');
+var gulp = require('gulp'),
+  gutil = require('gulp-util'),
+  coffee = require('gulp-coffee');
+
+  var coffeeSources = ['components/coffee/tagline.coffee']
 
 gulp.task('log',function(){
   gutil.log('workflows are awesome!')
-})
+});
+
+gulp.task('coffee',function(){
+  gulp.src('components/coffee/tagline.coffee')
+  .pipe(coffee({bare:true})
+    .on('error',gutil.log))
+    .pipe(gulp.dest('components/scripts'))
+});
