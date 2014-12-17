311378eb08dd4814dc85e6c3f0d21cec6496be27
cdaniels <cdaniels@marlboro.edu>
2014-12-16 21:36:29 -0500

    added iage compression

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,9 @@ connect = require('gulp-connect'),
 gulpif = require('gulp-if'),
 uglify = require('gulp-uglify'),
 minifyHTML = require('gulp-minify-html'),
-jsonminify = require('gulp-jsonminify')
+jsonminify = require('gulp-jsonminify'),
+imagemin = require('gulp-imagemin'),
+pngcrush = require('imagemin-pngcrush'),
 concat = require('gulp-concat');
 
 
@@ -92,6 +94,7 @@ gulp.task('watch',function(){
   gulp.watch('components/sass/*.scss',['compass']);
   gulp.watch('builds/development/*.html',['html']);
   gulp.watch('builds/development/*.json',['json']);
+  gulp.watch('build/development/images/**/*.*',['images']);
 });
 
 //fire up server
@@ -107,7 +110,19 @@ gulp.task('html',function(){
   gulp.src('builds/development/*.html')
   .pipe(gulpif(env === 'production',minifyHTML()))
   .pipe(gulpif(env === 'production',gulp.dest(outputDir)))
-    .pipe(connect.reload())
+  .pipe(connect.reload())
+});
+
+//image processing task
+gulp.task('images',function(){
+  gulp.src('builds/development/images/**/*.*')
+  .pipe(gulpif(env === 'production',imagemin({
+    progressive:true,
+    svgoPlugins:[{removeViewBox:false}],
+    use: [pngcrush()]
+  })))
+  .pipe(gulpif(env === 'production',gulp.dest(outputDir + 'images')))
+  .pipe(connect.reload())
 });
 
 //json processing code
@@ -120,6 +135,6 @@ gulp.task('json',function(){
 
 
 
-gulp.task('default',['log','html','json','coffee','js','compass','connect','watch']);
+gulp.task('default',['log','html','json','coffee','js','compass','images','connect','watch']);
 
 
