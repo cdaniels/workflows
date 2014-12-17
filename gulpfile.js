b2b8a52e1ff40d15479136c9fd517800b2959d79
cdaniels <cdaniels@marlboro.edu>
2014-12-16 19:57:07 -0500

    added static file monitoring

--- gulpfile.js
+++ gulpfile.js
@@ -18,6 +18,8 @@ var sassSources = ['components/sass/style.scss']
 gulp.task('log',function(){
   gutil.log('workflows are awesome!')
 });
+var htmlSources = ['builds/development/*.html']
+var jsonSources = ['builds/development/js/*.json']
 
 //compile coffee script
 gulp.task('coffee',function(){
@@ -52,6 +54,8 @@ gulp.task('watch',function(){
   gulp.watch(coffeeSources,['coffee']);
   gulp.watch(jsSources,['js']);
   gulp.watch('components/sass/*.scss',['compass']);
+  gulp.watch(htmlSources,['html']);
+  gulp.watch(jsonSources,['json']);
 });
 
 //fire up server
@@ -62,6 +66,18 @@ gulp.task('connect',function() {
   })
 });
 
-gulp.task('default',['coffee','js','compass','connect','watch']);
+gulp.task('html',function(){
+  gulp.src(htmlSources)
+    .pipe(connect.reload())
+});
+
+gulp.task('json',function(){
+  gulp.src(jsonSources)
+    .pipe(connect.reload())
+});
+
+
+
+gulp.task('default',['html','json','coffee','js','compass','connect','watch']);
 
 
